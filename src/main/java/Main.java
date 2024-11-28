7ebe15ab74205a9c5a3dc1600bcca0784be288ae
Aleksandr Borodin <aleksandr_borodin@senla.eu>
2024-11-28 16:43:18 +0300

    clean main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,12 @@
-import sun.misc.Resource;
-
-import java.io.*;
-import java.net.URI;
+import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.channels.SeekableByteChannel;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.*;
 
 /**
  * Author: Aleksandr Borodin
  * Creation date: 11/1/24
  */
 public class Main {
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args) {
         IO io = new IO();
         NIO nio = new NIO();
 
