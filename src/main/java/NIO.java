d0acd975301e60cf7841582bbc0d6ead041f5f60
Aleksandr Borodin <aleksandr_borodin@senla.eu>
2025-02-03 14:50:12 +0300

    added selector test

--- src/main/java/NIO.java
+++ src/main/java/NIO.java
@@ -1,11 +1,12 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.RandomAccessFile;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.net.URI;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
-import java.nio.channels.AsynchronousFileChannel;
-import java.nio.channels.FileChannel;
-import java.nio.channels.SeekableByteChannel;
+import java.nio.channels.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -13,6 +14,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.concurrent.Future;
 
 /**
@@ -55,19 +57,48 @@ public class NIO implements Runnable{
             }
 
             //Async file read
+            File file = new File("./tempFile.txt");
             System.out.println("NIO: Async file read");
-            try (AsynchronousFileChannel file = AsynchronousFileChannel.open(Paths.get(URI.create("./tempFile.txt")))) {
+            try (AsynchronousFileChannel asyncFile = AsynchronousFileChannel.open(file.toPath())) {
                 ByteBuffer buffer = ByteBuffer.allocate(255);
-                Future<Integer> future = file.read(buffer, 0);
+                Future<Integer> future = asyncFile.read(buffer, 0);
 
                 while (!future.isDone()) {
                     System.out.println("File is not loaded");
                 }
 
                 String fileStr = new String(buffer.array(), StandardCharsets.UTF_8);
-                System.out.println(fileStr);
+                int nIndex = fileStr.indexOf('\n');
+                System.out.println(fileStr.substring(0, nIndex));
             }
 
+            //Selector test
+
+            try (Selector selector = Selector.open()) {
+                ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+                serverSocketChannel.bind(new InetSocketAddress(7000));
+                serverSocketChannel.configureBlocking(false);
+                serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+                SocketChannel clientChannel = SocketChannel.open();
+                clientChannel.configureBlocking(false);
+                clientChannel.connect(new InetSocketAddress(7000));
+                clientChannel.register(selector, SelectionKey.OP_CONNECT);
+
+                selector.select();
+
+                Iterator<SelectionKey> iterator = selector.keys().iterator();
+
+                iterator.forEachRemaining((key) -> {
+                    if (key.isAcceptable()) {
+                        System.out.println("Server accepted new connection");
+                    }
+
+                    if (key.isConnectable()) {
+                        System.out.println("Client connected to server");
+                    }
+                });
+            }
 
         } catch (Exception e) {
             System.out.print(Arrays.toString(e.getStackTrace()));
